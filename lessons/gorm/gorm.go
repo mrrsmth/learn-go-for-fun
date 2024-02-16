bde2464c24e6e06f7670085eb40820b5d8c0795e
alxs1dd93 <alxs1dd93@gmail.com>
2024-02-17 00:55:48 +0700

    product db gorm

--- lessons/gorm/gorm.go
+++ lessons/gorm/gorm.go
@@ -16,95 +16,15 @@ type Product struct {
 	Price   int
 }
 
-func Up(db gorm.DB) {
-	updatedProduct := Product{
-		Model:   "Ноутбук Pro",
-		Company: "ASUS",
-		Price:   2999,
-	}
-
-	err := UpdateProduct(db, 11, updatedProduct)
+func CreateProduct(db *gorm.DB, newProduct Product) error {
+	err := db.Create(&newProduct).Error
 	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func Gorm() {
-	// Открываем соединение с базой данных
-	dsn := "root:123456@tcp(localhost:3306)/productdb?parseTime=true"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info),
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Получаем объект sql.DB
-	sqlDB, err := db.DB()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer sqlDB.Close()
-
-	// Создаем таблицу "products", если она еще не существует
-	// err = db.AutoMigrate(&Product{})
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// Создаем продукт по ID
-	createdProduct := Product{ID: 1, Model: "Ноутбук", Company: "ASUS", Price: 1999}
-	err = db.Create(&createdProduct).Error
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Получаем продукт по ID
-	var fetchedProduct Product
-	err = db.First(&fetchedProduct, 1).Error
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Полученный продукт: %+v\n", fetchedProduct)
-
-	// Обновляем продукт по ID
-	updatedProduct := Product{ID: 1, Model: "Обновленный ноутбук", Company: "ASUS", Price: 2499}
-	err = db.Save(&updatedProduct).Error
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Удаляем продукт по ID
-	err = DeleteProduct(db, 1)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Вызываем функцию Up для обновления продукта по ID
-	Up(*db)
-
-	// Выводим финальный результат в консоль
-	var products []Product
-	err = db.Find(&products).Error
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Итоговая база данных:")
-	for _, p := range products {
-		fmt.Printf("%+v\n", p)
-	}
-}
-
-func DeleteProduct(db *gorm.DB, id int) error {
-	product := Product{}
-	result := db.Where("id = ?", id).Delete(&product)
-	if result.Error != nil {
-		return result.Error
+		return err
 	}
 	return nil
 }
 
-func UpdateProduct(db gorm.DB, id int, updatedProduct Product) error {
+func UpdateProduct(db *gorm.DB, id int, updatedProduct Product) error {
 	product := Product{}
 	result := db.Where("id = ?", id).First(&product)
 	if result.Error != nil {
@@ -121,6 +41,93 @@ func UpdateProduct(db gorm.DB, id int, updatedProduct Product) error {
 	}
 
 	return nil
+}
+
+func DeleteProduct(db *gorm.DB, id int) error {
+	product := Product{}
+	result := db.Where("id = ?", id).Delete(&product)
+	if result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+func GetProduct(db *gorm.DB, id int) (Product, error) {
+	product := Product{}
+	result := db.First(&product, id)
+	if result.Error != nil {
+		return Product{}, result.Error
+	}
+	return product, nil
+}
+
+func GetNextProductID(db *gorm.DB) (int, error) {
+	var product Product
+	result := db.Order("id desc").First(&product)
+	if result.Error != nil {
+		return 0, result.Error
+	}
+	return product.ID + 1, nil
+}
+
+func Gorm() {
+	dsn := "root:123456@tcp(localhost:3306)/productdb?parseTime=true"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer sqlDB.Close()
+
+	// createdProduct := Product{ID: 1, Model: "Ноутбук", Company: "ASUS", Price: 1999}
+	// err = CreateProduct(db, createdProduct)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	id, err := GetNextProductID(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	newProduct := Product{ID: id, Model: "PupoBook", Company: "PupoPro", Price: 1555999}
+	err = CreateProduct(db, newProduct)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fetchedProduct, err := GetProduct(db, 1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Полученный продукт: %+v\n", fetchedProduct)
+
+	updatedProduct := Product{Model: "Обновленный ноутбук", Company: "ASUS", Price: 2499}
+	err = UpdateProduct(db, 1, updatedProduct)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// err = DeleteProduct(db, 10)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	var products []Product
+	err = db.Find(&products).Error
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Итоговая база данных:")
+	for _, p := range products {
+		fmt.Printf("%+v\n", p)
+	}
 }
 
 func GormStart() {
